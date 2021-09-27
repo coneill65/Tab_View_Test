c4e1f93bb0cda06c75ec5c4fc1d79a99ac56c94c
colin oneill <coneill65@students.mchenry.edu>
2021-09-27 15:07:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tabviewtest/ui/settings/SettingsFragment.kt
@@ -0,0 +1,32 @@
+package com.example.tabviewtest.ui.settings
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import com.example.tabviewtest.R
+import com.example.tabviewtest.ui.notifications.NotificationsViewModel
+
+class SettingsFragment : Fragment() {
+
+    private lateinit var settingsViewModel: SettingsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        settingsViewModel =
+            ViewModelProvider(this).get(SettingsViewModel::class.java)
+        val root = inflater.inflate(R.layout.settings_fragment, container, false)
+        val textView: TextView = root.findViewById(R.id.text_notifications)
+        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
